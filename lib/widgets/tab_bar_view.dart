dda309c22ac1bb60427c64b0439cb98d6eafcc06
tsering.sherpa <tsering.sherpa@info.com.np>
2024-03-22 15:29:32 +0545

    total amount added

--- lib/widgets/tab_bar_view.dart
+++ lib/widgets/tab_bar_view.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coffeeapp/widgets/transaction_list.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TypeTabBar extends StatefulWidget {
   TypeTabBar(
@@ -17,37 +20,108 @@ class TypeTabBar extends StatefulWidget {
 }
 
 class _TypeTabBarState extends State<TypeTabBar> {
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+  var type = 'credit';
+  var totalAmount = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Query query = FirebaseFirestore.instance
+        .collection("users")
+        .doc(userId)
+        .collection('transactions')
+        .orderBy('timeStamp', descending: true)
+        .where('monthyear', isEqualTo: widget.monthYear)
+        .where('type', isEqualTo: type)
+        .where('isAdvance', isEqualTo: widget.isAdvance)
+        .where('isDelete', isEqualTo: false);
+
+    if (widget.category != '0') {
+      query = query.where("categoryId", isEqualTo: widget.category);
+    }
+
+    query.get().then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          totalAmount += int.parse(doc["amount"]);
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
         child: DefaultTabController(
             length: 2,
-            child: Column(
+            child: Stack(
               children: [
-                TabBar(tabs: [
-                  Tab(
-                    text: "Credit",
+                Container(
+                  child: Column(
+                    children: [
+                      TabBar(
+                          onTap: (value) => {
+                                if (value == 0)
+                                  {
+                                    setState(() {
+                                      type = 'credit';
+                                    })
+                                  }
+                                else
+                                  {
+                                    setState(() {
+                                      type = 'debit';
+                                    })
+                                  }
+                              },
+                          tabs: [
+                            Tab(
+                              text: "Credit",
+                            ),
+                            Tab(
+                              text: "Debit",
+                            ),
+                          ]),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Expanded(
+                          child: TabBarView(children: [
+                        TransactionList(
+                            category: widget.category,
+                            type: type,
+                            monthYear: widget.monthYear,
+                            isAdvance: widget.isAdvance),
+                        TransactionList(
+                            category: widget.category,
+                            type: type,
+                            monthYear: widget.monthYear,
+                            isAdvance: widget.isAdvance),
+                      ]))
+                    ],
                   ),
-                  Tab(
-                    text: "Debit",
-                  ),
-                ]),
-                SizedBox(
-                  height: 5,
                 ),
-                Expanded(
-                    child: TabBarView(children: [
-                  TransactionList(
-                      category: widget.category,
-                      type: 'credit',
-                      monthYear: widget.monthYear,
-                      isAdvance: widget.isAdvance),
-                  TransactionList(
-                      category: widget.category,
-                      type: 'debit',
-                      monthYear: widget.monthYear,
-                      isAdvance: widget.isAdvance),
-                ]))
+                Positioned(
+                  bottom: 0.0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        'Total Amount: Rs ${NumberFormat('##,##,##,##,###').format(totalAmount)}',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             )));
   }
